18e62adb9cf6a222e1f5b8c9c03b2337becce69f
Tomasz Jędrzejewski <nightstorm@tlen.pl>
2016-11-10 18:44:42 +0100

    Discussion module: changing the database schema

--- db/schema/13.sql
+++ db/schema/13.sql
@@ -7,11 +7,13 @@ CREATE TABLE IF NOT EXISTS `cantiga_discussion_channels` (
   `description` varchar(250) NOT NULL DEFAULT '',
   `color` varchar(30) NOT NULL DEFAULT 'green',
   `icon` varchar(30) NOT NULL,
-  `lastPostTime` int(11) NOT NULL,
   `projectVisible` tinyint(1) NOT NULL,
   `groupVisible` tinyint(1) NOT NULL,
   `areaVisible` tinyint(1) NOT NULL,
-  `separate` tinyint(1) NOT NULL,
+  `projectPosting` int(11) NOT NULL,
+  `groupPosting` int(11) NOT NULL,
+  `areaPosting` int(11) NOT NULL,
+  `subchannelLevel` tinyint(2) NOT NULL,
   `enabled` tinyint(1) NOT NULL DEFAULT '1',
   PRIMARY KEY (`id`),
   KEY `projectId` (`projectId`)
@@ -19,21 +21,32 @@ CREATE TABLE IF NOT EXISTS `cantiga_discussion_channels` (
 
 CREATE TABLE IF NOT EXISTS `cantiga_discussion_posts` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
-  `channelId` int(11) NOT NULL,
+  `subchannelId` int(11) NOT NULL,
   `authorId` int(11) NOT NULL,
-  `entityId` int(11) NOT NULL,
   `createdAt` int(11) NOT NULL,
   `content` text,
   PRIMARY KEY (`id`),
-  KEY `channelId` (`channelId`),
-  KEY `authorId` (`authorId`),
-  KEY `entityId` (`entityId`)
+  KEY `channelId` (`subchannelId`),
+  KEY `authorId` (`authorId`)
 ) ENGINE=InnoDB  DEFAULT CHARSET=utf8;
 
-ALTER TABLE `cantiga_discussion_posts`
-  ADD CONSTRAINT `cantiga_discussion_posts_ibfk_1` FOREIGN KEY (`channelId`) REFERENCES `cantiga_discussion_channels` (`id`) ON DELETE CASCADE ON UPDATE CASCADE,
-  ADD CONSTRAINT `cantiga_discussion_posts_ibfk_2` FOREIGN KEY (`authorId`) REFERENCES `cantiga_users` (`id`) ON DELETE CASCADE ON UPDATE CASCADE,
-  ADD CONSTRAINT `cantiga_discussion_posts_ibfk_3` FOREIGN KEY (`entityId`) REFERENCES `cantiga_entities` (`id`) ON DELETE CASCADE ON UPDATE CASCADE;
+CREATE TABLE IF NOT EXISTS `cantiga_discussion_subchannels` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `channelId` int(11) NOT NULL,
+  `entityId` int(11) NOT NULL,
+  `lastPostTime` int(11) DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  KEY `channelId` (`channelId`),
+  KEY `entityId` (`entityId`)
+) ENGINE=InnoDB  DEFAULT CHARSET=utf8 COLLATE=utf8_general_ci;
 
 ALTER TABLE `cantiga_discussion_channels`
   ADD CONSTRAINT `cantiga_discussion_channels_ibfk_1` FOREIGN KEY (`projectId`) REFERENCES `cantiga_projects` (`id`) ON DELETE CASCADE ON UPDATE CASCADE;
+
+ALTER TABLE `cantiga_discussion_posts`
+  ADD CONSTRAINT `cantiga_discussion_posts_ibfk_1` FOREIGN KEY (`subchannelId`) REFERENCES `cantiga_discussion_subchannels` (`id`) ON DELETE CASCADE ON UPDATE CASCADE,
+  ADD CONSTRAINT `cantiga_discussion_posts_ibfk_2` FOREIGN KEY (`authorId`) REFERENCES `cantiga_users` (`id`) ON DELETE CASCADE ON UPDATE CASCADE;
+
+ALTER TABLE `cantiga_discussion_subchannels`
+  ADD CONSTRAINT `cantiga_discussion_subchannels_ibfk_1` FOREIGN KEY (`channelId`) REFERENCES `cantiga_discussion_channels` (`id`) ON DELETE CASCADE ON UPDATE CASCADE,
+  ADD CONSTRAINT `cantiga_discussion_subchannels_ibfk_2` FOREIGN KEY (`entityId`) REFERENCES `cantiga_entities` (`id`) ON DELETE CASCADE ON UPDATE CASCADE;
