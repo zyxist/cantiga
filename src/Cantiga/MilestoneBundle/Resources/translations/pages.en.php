1ba9eca7bfbb8ebc8a631226b9dd85bbeaa9a0fe
nightstorm@tlen.pl <nightstorm@tlen.pl>
2016-12-26 16:46:20 +0100

    Updating the UI and translation in the milestone bundle.

--- src/Cantiga/MilestoneBundle/Resources/translations/pages.en.php
+++ src/Cantiga/MilestoneBundle/Resources/translations/pages.en.php
@@ -1,6 +1,8 @@
 <?php
 return [
 	'Milestones' => 'Milestones',
+	'Create a new milestone' => 'Create a new milestone',
+	'Edit milestone: 0' => 'Edit milestone: 0',
 	'Manage the milestones for groups and areas' => 'Manage the milestones for groups and areas',
 	'View and manage the progress' => 'View and manage the progress',
 	'View the progress of areas and groups' => 'View the progress of areas and groups',
